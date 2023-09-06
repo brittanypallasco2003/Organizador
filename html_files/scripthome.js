5e4a1b5fcf6353ade61bf925643a928dbdd069f5
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-09-05 19:38:47 -0500

    completado de actividades listo

--- html_files/scripthome.js
+++ html_files/scripthome.js
@@ -1,72 +1,79 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const lista = $("#lista");
+  const tareasRealizadas = $("#tareas_realizadas");
 
-  $("#formulario").submit(function(event) {
+  $("#formulario").submit(function (event) {
     event.preventDefault();
 
-    
-
     // Clear form fields
-    $("#act_realizar").val('');
-    $("#tiempo").val('');
+    $("#act_realizar").val("");
+    $("#tiempo").val("");
     $("input[name='tipo_act']").prop('checked', false);
-    $("#act1").val('');
-    $("#act2").val('');
+    $("#act1").val("");
+    $("#act2").val("");
   });
 
+  let tareaCounter = 0;
   function agregarTarea(act_realizar, fecha, tipo, act1, act2) {
-    var act_realizar = $("#act_realizar").val();
-    var fecha = $("#tiempo").val();
-    var tipo = $("input[name='tipo_act']:checked").val();
-    var act1 = $("#act1").val();
-    var act2 = $("#act2").val();
-    const conjuntoElemento  = []
-    
+    tareaCounter++;
+
     const elemento = `
       <div class="content">
         <div class="tarjeta">
-          <li id="tarjeta">
+          <li>
             <p>Act: ${act_realizar}</p>
-            <p>Fecha limite: ${fecha}</p>
+            <p>Fecha límite: ${fecha}</p>
             <p>Tipo de actividad: ${tipo}</p>
             <h3>Detalles</h3>
-            <p><input type="checkbox" id="scales1" name="scales" checked/>
-            <label for="scales1">${act1}</label>
-            <input type="checkbox" id="scales2" name="scales" checked/>
-            <label for="scales2">${act2}</label></p>
+            <p>
+              <input type="checkbox" id="scales1" name="scales" checked />
+              <label for="scales1">${act1}</label>
+              <input type="checkbox" id="scales2" name="scales" checked />
+              <label for="scales2">${act2}</label>
+            </p>
             <img src="/img/reloj.png" width="45" height="45">
-            <button type="button">Realizado</button>
-            <button type="button" id=eliminar>Eliminar</button>
+            <button type="button" class="realizado-btn">Realizado</button>
+            <button type="button" class="eliminar-btn">Eliminar</button>
           </li>
         </div>
       </div>
     `;
-    console.log(elemento);
+
     lista.append(elemento);
   }
 
-  $("#botoningresar").click(function() {
-    const tarea = $("#act_realizar").val();
+  $("#botoningresar").click(function () {
+    const act_realizar = $("#act_realizar").val();
+    const fecha = $("#tiempo").val();
+    const tipo = $("input[name='tipo_act']:checked").val();
+    const act1 = $("#act1").val();
+    const act2 = $("#act2").val();
 
-    if (tarea) {
-      agregarTarea(tarea);
-      $("#act_realizar").val('');
+    if (act_realizar) {
+      agregarTarea(act_realizar, fecha, tipo, act1, act2);
+      $("#act_realizar").val("");
     }
+  });
+
+  $(document).on("click", ".eliminar-btn", function () {
+    $(this).closest(".content").remove();
+  });
+
+  $(document).on("click", ".realizado-btn", function () {
+    const tareaCompletada = $(this).closest(".content");
+    tareaCompletada.appendTo(tareasRealizadas);
+    $(this).hide();
   });
 });
 
-
-
 //eliminar lo creado de actividad
-$(document).on("click", "#eliminar", function() {
+$(document).on("click", "#eliminar", function () {
   $(this).closest("div").remove();
 });
 
-
-
-
-
+//muestra el formulario
 function toggleContenido() {
-  var contenido = document.getElementById('contenido_oculto');
-  contenido.style.display = contenido.style.display === 'none' ? 'block' : 'none';
+  var contenido = document.getElementById("contenido_oculto");
+  contenido.style.display =
+    contenido.style.display === "none" ? "block" : "none";
 }
