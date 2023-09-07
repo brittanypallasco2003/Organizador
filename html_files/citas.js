3d571ca05f4407a88ca6476f02b06de954df3968
DavidPK8 <alejodavicho8@gmail.com>
2023-09-06 21:02:34 -0500

    Cambios

--- html_files/citas.js
+++ html_files/citas.js
@@ -1,15 +1,17 @@
+// Inicializar el calendario después de agregar tareas
 const calendarElement = document.getElementById('calendar');
 
 // Crear un objeto para almacenar las citas
 const citas = [];
 
+// ... (Agrega tareas a la matriz 'citas' usando la función agregarTarea)
+
 // Configurar y mostrar el calendario
 const calendar = new FullCalendar.Calendar(calendarElement, {
     initialView: 'dayGridMonth',
-    events: citas
+    events: citas // Utiliza la matriz 'citas' para mostrar eventos
 });
 calendar.render();
-
 
 
 
